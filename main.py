278f10fe96e1cbfdaef5fc3109064037cef3478f
jackwillcode <llondenbiz@yahoo.com>
2022-02-19 21:52:10 -0600

    Added exercise to pick random quote from file and email it on a selected weekday

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
+# import smtplib
+#
+# my_email = "jackiewillcode@gmail.com"
+# my_password = "b&66gKCOyhnt&0#Oq#@O"
+# googleSMTP = "smtp.gmail.com"
+# yahooSMTP = "smtp.mail.yahoo.com"
+#
+# # Smtplib object to connect to our email provider's smtp email server
+# with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+#     # Secure our connection to email server using tls
+#     connection.starttls()
+#     # Login to email
+#     connection.login(user=my_email, password=my_password)
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs="jackwillcode@yahoo.com",
+#                         msg="Subject:Hello\n\nThis is the body of my email, again."
+#                         )
+
 import smtplib
+import datetime as dt
+import random
 
-my_email = "jackiewillcode@gmail.com"
-my_password = "b&66gKCOyhnt&0#Oq#@O"
-googleSMTP = "smtp.gmail.com"
-yahooSMTP = "smtp.mail.yahoo.com"
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-# Smtplib object to connect to our email provider's smtp email server
-with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-    # Secure our connection to email server using tls
-    connection.starttls()
-    # Login to email
-    connection.login(user=my_email, password=my_password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs="jackwillcode@yahoo.com",
-                        msg="Subject:Hello\n\nThis is the body of my email, again."
-                        )
+# Get current date and time
+now = dt.datetime.now()
+day_index = now.weekday()
+
+if days[day_index] == "Monday":
+    # Open and read all quotes from file line-by-line
+    with open("quotes.txt") as file_object:
+        quotes = file_object.readlines()
+
+    random_quote = random.choice(quotes)
+
+    my_email = "jackiewillcode@gmail.com"
+    my_password = "b&66gKCOyhnt&0#Oq#@O"
+    googleSMTP = "smtp.gmail.com"
+    yahooSMTP = "smtp.mail.yahoo.com"
+
+    # Smtplib object to connect to our email provider's smtp email server
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        # Secure our connection to email server using tls
+        connection.starttls()
+        # Login to email
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="jackwillcode@yahoo.com",
+                            msg=f"Subject:Monday Motivation Quote\n\n{random_quote}"
+                            )
